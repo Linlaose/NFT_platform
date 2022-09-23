81b678de396743da68728ef931c2723d9333df82
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-23 23:10:45 +0800

    新增套件：google-font、font-awesome、swiper

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { // 空格記得要兩種引號
+        'noto': ['"Noto Sans TC"',
+          '"-apple - system"',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Microsoft JhengHei"',
+          '"Helvetica Neue"',
+          'monospace',
+          'Arial',
+          'cursive',
+          'sans-serif'],
+        'paytone': ['"Paytone One"',
+          '"-apple - system"',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Microsoft JhengHei"',
+          '"Helvetica Neue"',
+          'monospace',
+          'Arial',
+          'cursive',
+          'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin'),
