78083652f8169bce5783149cc9b61076c77b7a36
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-23 23:06:46 +0800

    新增模組：Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  content: ["./app/**/*.{html,ejs,js}"],
+  content: [
+    "./app/**/*.{html,ejs,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [
+    require('flowbite/plugin'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
