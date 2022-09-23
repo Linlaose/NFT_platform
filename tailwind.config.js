e8275db2f58ca28330bb74e66a4123c67b910c7c
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-24 00:57:03 +0800

    更新(layout.ejs)：PC header、footer 完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1296px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: { // 空格記得要兩種引號
         'noto': ['"Noto Sans TC"',
@@ -26,6 +45,25 @@ module.exports = {
           'Arial',
           'cursive',
           'sans-serif'],
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+      fontSize: {
+        '32px': '2rem',
+      },
+      colors: {
+        projectWhite: {
+          100: "#fcfcfc",
+          200: "#f9f9f9",
+          300: "#f6f6f6",
+          400: "#f3f3f3",
+          500: "#f0f0f0", // 背景色
+          600: "#c0c0c0",
+          700: "#909090",
+          800: "#606060",
+          900: "#303030"
+        },
       },
     },
   },
