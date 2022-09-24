e222ee2d5111861a2e2652f6713b616c93bd7eaa
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-24 18:59:00 +0800

    更新(layout.ejs)：header RWD 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,4 @@
+/* 漢堡 */
 const burgerBox = document.querySelector('#burger-box');
 const burgerTopLine = document.querySelector('#burger-top-line');
 const burgerMidLine = document.querySelector('#burger-mid-line');
@@ -5,6 +6,7 @@ const burgerBtmLine = document.querySelector('#burger-btm-line');
 const burgerNav = document.querySelector('#burger-nav');
 const burgerNavMask = document.querySelector('#burger-nav-mask');
 const xMark = document.querySelector('#xmark');
+
 const burgerMenu = () => {
   burgerBox.classList.add('translate-x-full');
   xMark.classList.remove('opacity-0', '-z-10');
@@ -21,6 +23,7 @@ const burgerNavHide = () => {
   burgerNav.classList.add('h-0', 'opacity-0');
   burgerNavMask.classList.add('opacity-0');
 };
+
 burgerBox.addEventListener('click', () => {
   burgerMenu();
   burgerNavShow();
@@ -28,4 +31,38 @@ burgerBox.addEventListener('click', () => {
 xMark.addEventListener('click', () => {
   burgerCancel();
   burgerNavHide();
+});
+/* 漢堡 end */
+
+/* search */
+const btnSearch = document.querySelector('#btn-search');
+const burger = document.querySelector('#burger');
+const logoBox = document.querySelector('#logo-box');
+const searchField = document.querySelector('#serach-field');
+const nav = document.querySelector('#nav');
+const navSearch = document.querySelector('#nav-search');
+const btnSearchCancel = document.querySelector('#btn-search-cancel');
+
+const search = () => {
+  burger.classList.add('hidden');
+  logoBox.classList.add('hidden');
+  nav.classList.add('w-full');
+  navSearch.classList.add('w-full', 'justify-between');
+  searchField.classList.remove('hidden');
+  btnSearchCancel.classList.remove('hidden');
+};
+const searchCancel = () => {
+  burger.classList.remove('hidden');
+  logoBox.classList.remove('hidden');
+  nav.classList.remove('w-full');
+  navSearch.classList.remove('w-full', 'justify-between');
+  searchField.classList.add('hidden');
+  btnSearchCancel.classList.add('hidden');
+};
+
+btnSearch.addEventListener('click', () => {
+  search();
+});
+btnSearchCancel.addEventListener('click', () => {
+  searchCancel();
 });
