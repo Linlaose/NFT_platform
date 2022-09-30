11ae2b15920f435b526aea64eb9ae37246dde83b
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-30 15:17:13 +0800

    新增(products.html)：完成收藏品區塊

--- app/assets/js/products.js
+++ app/assets/js/products.js
@@ -1,3 +1,4 @@
+// tabs
 // 變數
 const tabArtwork = document.querySelector('#tab-artwork');
 const tabCollection = document.querySelector('#tab-collection');
@@ -30,8 +31,32 @@ const collectionBorderShow = (() => {
 if (tabArtwork) {
   tabArtwork.addEventListener('click', () => {
     artworkBorderShow();
+    artworkShow();
   })
   tabCollection.addEventListener('click', () => {
     collectionBorderShow();
+    artworkHide();
   })
 }
+// tabs end
+
+// content
+// 變數
+const artwork = document.querySelector('#artwork');
+const collection = document.querySelector('#collection');
+const productSection = document.querySelector('#productSection');
+
+// 方法
+const artworkHide = () => {
+  artwork.classList.add('opacity-0', 'absolute', '-z-10', 'top-0');
+  collection.classList.remove('opacity-0', 'absolute', 'top-0', '-z-10', 'scale-0');
+  productSection.classList.remove('lg:pb-32');
+  productSection.classList.add('lg:pb-8');
+}
+const artworkShow = () => {
+  artwork.classList.remove('opacity-0', 'absolute', '-z-10', 'top-0');
+  collection.classList.add('opacity-0', 'absolute', 'top-0', '-z-10', 'scale-0');
+  productSection.classList.remove('lg:pb-8');
+  productSection.classList.add('lg:pb-32');
+}
+// content end
